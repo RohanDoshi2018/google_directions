4d368e552244385df378cbc12fbb9c55a3daa792
RohanDoshi2018 <rkdoshi@princeton.edu>
2015-08-30 01:30:11 -0400

    cleaned formatting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
- * This NPM Module serves as a Node Wrapper for asynchronously 
- * querying the Google Directions API.
- */
+* This NPM Module serves as a Node Wrapper for asynchronously 
+* querying the Google Directions API.
+*/
 
 var request = require('request');
 var qs = require('query-string');
@@ -20,88 +20,83 @@ function validateInput(params, cb) {
 		throw new Error("params.key is required");
 }
 
- module.exports = {
- 	getDirections: function(params, cb) {
- 		// validate inputs to npm module
- 		validateInput(params, cb);
+module.exports = {
+	getDirections: function(params, cb) {
+		// validate inputs to npm module
+		validateInput(params, cb);
 
- 		// build query URL
-		var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
+		// build query URL
+	var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
 
-		// make request to google server
-		request(url, function(err, res, body) {
-			return cb(err, JSON.parse(body));
-		});
+	// make request to google server
+	request(url, function(err, res, body) {
+		return cb(err, JSON.parse(body));
+	});
 
- 	},
- 	getDirectionSteps: function(params, cb) {
- 		// validate inputs to npm module
- 		validateInput(params, cb);
+	},
+	getDirectionSteps: function(params, cb) {
+		// validate inputs to npm module
+		validateInput(params, cb);
 
- 		// build query URL
-		var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
-		// make request to google server
-		request(url, function(err, res, body) {
-			return cb(err, JSON.parse(body).routes[0].legs[0].steps);
-		});
+		// build query URL
+	var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
+	// make request to google server
+	request(url, function(err, res, body) {
+		return cb(err, JSON.parse(body).routes[0].legs[0].steps);
+	});
 
- 	},
- 	getDistance: function(params, cb) {
- 		// validate inputs to npm module
- 		validateInput(params, cb);
+	},
+	getDistance: function(params, cb) {
+		// validate inputs to npm module
+		validateInput(params, cb);
 
- 		// build query URL
-		var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
+		// build query URL
+	var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
 
-		// make request to google server
-		request(url, function(err, res, body) {
-			return cb(err, JSON.parse(body).routes[0].legs[0].distance.text);
-		});
+	// make request to google server
+	request(url, function(err, res, body) {
+		return cb(err, JSON.parse(body).routes[0].legs[0].distance.text);
+	});
 
- 	},
- 	getDuration: function(params, cb) {
- 		// validate inputs to npm module
- 		validateInput(params, cb);
+	},
+	getDuration: function(params, cb) {
+		// validate inputs to npm module
+		validateInput(params, cb);
 
- 		// build query URL
-		var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
+		// build query URL
+	var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
 
-		// make request to google server
-		request(url, function(err, res, body) {
-			return cb(err, JSON.parse(body).routes[0].legs[0].duration.text);
-		});
+	// make request to google server
+	request(url, function(err, res, body) {
+		return cb(err, JSON.parse(body).routes[0].legs[0].duration.text);
+	});
 
- 	},
- 	getOriginAddress: function(params, cb) {
- 		// validate inputs to npm module
- 		validateInput(params, cb);
+	},
+	getOriginAddress: function(params, cb) {
+		// validate inputs to npm module
+		validateInput(params, cb);
 
- 		// build query URL
-		var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
+		// build query URL
+	var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
 
-		// make request to google server
-		request(url, function(err, res, body) {
-			return cb(err, JSON.parse(body).routes[0].legs[0].start_address);
-		});
+	// make request to google server
+	request(url, function(err, res, body) {
+		return cb(err, JSON.parse(body).routes[0].legs[0].start_address);
+	});
 
- 	},
- 	getDestinationAddress: function(params, cb) {
- 		// validate inputs to npm module
- 		validateInput(params, cb);
+	},
+	getDestinationAddress: function(params, cb) {
+		// validate inputs to npm module
+		validateInput(params, cb);
 
- 		// build query URL
-		var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
+		// build query URL
+	var url = "https://maps.googleapis.com/maps/api/directions/json?" + qs.stringify(params);
 
-		// make request to google server
-		request(url, function(err, res, body) {
-			return cb(err, JSON.parse(body).routes[0].legs[0].end_address);
-		});
+	// make request to google server
+	request(url, function(err, res, body) {
+		return cb(err, JSON.parse(body).routes[0].legs[0].end_address);
+	});
 
- 	}
- };
- // re-tag to higher version, and redeploy
+	}
+};
 
-// https://quickleft.com/blog/creating-and-publishing-a-node-js-module/
-
-// validation --> parameters good, google server working, request working, there is a valid response
-// fix folder structure
